464f66b47f884584009c05c00cff520b4c6d0f4a
JasonZheng02 <Jzheng02@stuy.edu>
2019-05-13 23:24:09 -0400

    done

--- Fib.java
+++ Fib.java
@@ -11,29 +11,54 @@ public class Fib {
       @return the nth Fibonacci number
               by implementing the recurrence relation
      */
-    public ?? long fib_recurrence( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_recurrence( int n) {
+        if      (n == 0) return 0;
+        else if (n == 1) return 1;
+        else {
+            return fib_recurrence(n - 1) +
+                   fib_recurrence(n - 2);
+        }
     }
-     /* These are class / instance methods because...
+     /* These are class methods because the user does not need 
+        an instance of the class to run the method
     */
-
+    /* Time complexity:
+       Consider the size of the problem to be less than max value of long
+       
+       As the proxy for the time required, count comparisons
+       
+       Then cost of the the recurrence algorithm
+       grows in a fibonacci sequence starting with 1 2
+       as the size of the problem increases,
+       because of the recursive case
+     */
 
     /** 
       @return the nth Fibonacci number
               calculated via the 8th-grade algorithm
      */
-    public ?? long fib_grade8( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_grade8( int n) {
+        int fib0 = 0;
+        int fib1 = 1;
+        if      (n == 0) return fib0;
+        else if (n == 1) return fib1;
+        for (int counter = 2; counter <= n; counter++) {
+            if (counter % 2 == 0)
+                fib0 = fib0 + fib1;
+            else
+                fib1 = fib0 + fib1;
+        }
+        return Math.max(fib0, fib1);
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be less than max value of long
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count comparisons
        
-       Then cost of the the recurrence algorithm
-       grows ?? 
+       Then cost of the the grade8 algorithm
+       grows in a linear way starting at 1 slope 1
        as the size of the problem increases,
-       because ??
+       because of the for loop doing one comparison per increase in n
      */
 
 
@@ -43,17 +68,19 @@ public class Fib {
               Type is double so that this exercise can
               ignore rounding issues.
      */
-    public ?? double fib_Binet( int n) {
-        return -2; // invalid Fibonacci number
+    public static double fib_Binet( int n) {
+        double root5 = Math.sqrt(5);
+        double phi = (Math.pow(((1 + root5) / 2),n) - Math.pow(((1 - root5) / 2),n));
+        return phi / root5;
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be less than max value of double
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count n exponent???
        
-       Then cost of the the recurrence algorithm
-       grows ?? 
+       Then cost of the the binet algorithm
+       grows constant
        as the size of the problem increases,
-       because ??
+       because just exponent increasing
      */
 }
